fb02ed4d62919ce6d2f417cbf663283ac3375be9
Dennis Gookin <degookin>
2019-12-05 14:31:32 -0500

    refactor to add 'new' route

--- app.js
+++ app.js
@@ -21,24 +21,24 @@ app.get("/", function(req, res){
 
 //YEAR ROUTES
 //2015
-app.get("/views/years/2015", function(req, res){
-    res.render("years/2015");
+app.get("/views/years/2015/index", function(req, res){
+    res.render("years/2015/index");
 });
 //2016
-app.get("/views/years/2016", function(req, res){
-    res.render("years/2016");
+app.get("/views/years/2016/index", function(req, res){
+    res.render("years/2016/index");
 });
 //2017
-app.get("/views/years/2017", function(req, res){
-    res.render("years/2017");
+app.get("/views/years/2017/index", function(req, res){
+    res.render("years/2017/index");
 });
 //2018
-app.get("/views/years/2018", function(req, res){
-    res.render("years/2018");
+app.get("/views/years/2018/index", function(req, res){
+    res.render("years/2018/index");
 });
    //2019
-app.get("/views/years/2019", function(req, res){
-    res.render("years/2019");
+app.get("/views/years/2019/index", function(req, res){
+    res.render("years/2019/index");
 });
 
 
@@ -68,9 +68,26 @@ app.get("/views/family/owen", function(req, res){
     res.render("family/owen");
 });
 
+//CREATE - add a new vacation
+app.post("/years/2019/index", function(req, res){
+    //res.send("POST ROUTE!!!")
+   var title = req.body.title;
+   var desc = req.body.desc;
+   var people = req.body.people;
+   var newVacation = {title: title, desc: desc, people: people};
+   Vacation.create(newVacation, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+           res.redirect("/years/2019/index")
+       }
+   })
+});
 
-
-
+//NEW
+app.get("/views/years/2019/new", function(req, res){
+    res.render("years/2019/new");
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("TRAVEL SERVER HAS STARTED!")
